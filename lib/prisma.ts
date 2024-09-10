4b55da901a53db7491c84909d241ab24363adafa
Justin Cairns <justincairnsuk@gmail.com>
2024-09-10 17:07:17 -0600

    postgres setup

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,3 @@
-import { PrismaClient } from '@prisma/client'
+import prisma from '../prisma/prisma'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
-
-export const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({
-        log: ['query'],
-    })
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export { prisma }
